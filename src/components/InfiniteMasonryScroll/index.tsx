35b42babe5915826d4aaae3993dab03161bbb577
3lang <675483520@qq.com>
2021-03-27 13:31:17 +0800

    feat: add pro-infinite-scroll

--- src/components/InfiniteMasonryScroll/index.tsx
+++ src/components/InfiniteMasonryScroll/index.tsx
@@ -2,7 +2,8 @@
 import type { MasonryCssProps } from '@/components/Masonry';
 import Masorny from '@/components/Masonry';
 import useRequest from '@ahooksjs/use-request';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useSetState, useUpdateEffect } from 'ahooks';
+import { useEffect, useRef } from 'react';
 import type { Props as InfiniteScrollProps } from 'react-infinite-scroll-component';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
@@ -10,15 +11,25 @@ type InfiniteMasonryScrollProps = {
   masonryProps?: Omit<MasonryCssProps, 'children'>;
 } & InfiniteScrollProps;
 
-export default ({ children, masonryProps, ...props }: InfiniteMasonryScrollProps) => {
+const InfiniteMasonryScroll = ({
+  children,
+  masonryProps,
+  ...props
+}: InfiniteMasonryScrollProps) => {
   return (
     <InfiniteScroll {...props}>
-      <Masorny {...masonryProps}>{children}</Masorny>
+      {masonryProps &&
+      typeof masonryProps.breakpointCols === 'number' &&
+      masonryProps.breakpointCols <= 1 ? (
+        children
+      ) : (
+        <Masorny {...masonryProps}>{children}</Masorny>
+      )}
     </InfiniteScroll>
   );
 };
 
-type useInfiniteMasonryReturnType<T> = { items: T[]; scrollProps: any };
+type useInfiniteMasonryReturnType<T> = { items: T[]; scrollProps: any; refresh: any };
 
 type ServiceResponseType = {
   list: any[];
@@ -29,25 +40,38 @@ type ServiceType = {
   ({ current, pageSize }: any): Promise<ServiceResponseType>;
 };
 
-export function useInfiniteMasonry<T>(service: ServiceType): useInfiniteMasonryReturnType<T> {
-  const [items, setItems] = useState<any[]>([]);
-  const [hasMore, setHasMore] = useState<boolean>(true);
-  const pagiRef = useRef<any>({ page: 0, pageSize: 10 });
+type OptsType = {
+  defaultParams?: Record<string, any>;
+  defaultPageSize?: number;
+  defaultCurrent?: number;
+};
+
+export function useInfiniteMasonry<T>(
+  service: ServiceType,
+  opts?: OptsType,
+): useInfiniteMasonryReturnType<T> {
+  const [state, setState] = useSetState({ items: [], hasMore: true });
+  const pagiRef = useRef<any>({
+    page: opts?.defaultCurrent || 1,
+    pageSize: opts?.defaultPageSize || 10,
+  });
   const { run, loading, ...rest } = useRequest(service, {
-    defaultParams: [{ ...pagiRef.current }],
     manual: true,
   });
 
-  const next = async () => {
-    if (loading || !hasMore) return;
+  const getData = async (pagi) => {
     try {
       const { type, msg, data }: any = await run({
-        page: Number(pagiRef.current.page) + 1,
-        pageSize: pagiRef.current.pageSize,
+        page: pagi.page,
+        pageSize: pagi.pageSize,
+        ...opts?.defaultParams,
       });
       if (type === 1) throw Error(msg);
-      setHasMore(data._page.page * 1 < data._page.totalPage);
-      setItems((v) => [...v, ...data._list]);
+      setState((v) => ({
+        items: pagi.page * 1 === 1 ? data._list : [...v.items, ...data._list],
+        hasMore: data._page.page * 1 < data._page.totalPage,
+      }));
+      data._page.page = data._page.page * 1 + 1;
       pagiRef.current = data._page;
     } catch (error) {
       // eslint-disable-next-line no-console
@@ -55,18 +79,150 @@ export function useInfiniteMasonry<T>(service: ServiceType): useInfiniteMasonryR
     }
   };
 
+  const next = () => {
+    if (loading || !state.hasMore) return;
+    getData(pagiRef.current);
+  };
+
+  const refresh = () => {
+    if (loading) return;
+    getData({ page: 1, pageSize: pagiRef.current?.pageSize || 10 });
+  };
+
   useEffect(() => {
     next();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const dataLength = useMemo(() => items.length, [items]);
+  const dataLength = () => state.items.length;
 
   const scrollProps = {
-    hasMore,
+    hasMore: state.hasMore,
     dataLength,
     next,
   };
 
-  return { items, loading, scrollProps, ...rest } as useInfiniteMasonryReturnType<T>;
+  return {
+    items: state.items,
+    loading,
+    scrollProps,
+    ...rest,
+    refresh,
+  } as useInfiniteMasonryReturnType<T>;
 }
+
+type ProInfiniteScrollProps<T> = {
+  itemRender: (item: T, index: number, itemArr: T[]) => React.ReactNode;
+  request: (
+    p: { current: number; pageSize: number } & Record<string, any>,
+  ) => Promise<{ list: T[]; total: number }>;
+  params?: Record<string, any>;
+  pagination?: {
+    defaultPageSize?: number;
+    defaultCurrent?: number;
+  };
+  pullDownToRefresh?:
+    | boolean
+    | {
+        threshold?: InfiniteScrollProps['pullDownToRefreshThreshold'];
+        content?: InfiniteScrollProps['pullDownToRefreshContent'];
+        releaseContent?: InfiniteScrollProps['releaseToRefreshContent'];
+      };
+} & Pick<InfiniteScrollProps, 'scrollThreshold' | 'loader' | 'endMessage'> &
+  Pick<MasonryCssProps, 'className' | 'columnClassName' | 'breakpointCols'>;
+
+export const ProInfiniteScroll: <T extends Record<string, any>>(
+  props: ProInfiniteScrollProps<T>,
+) => JSX.Element = ({
+  itemRender,
+  request,
+  params = {},
+  pagination = { defaultPageSize: 10, defaultCurrent: 1 },
+  scrollThreshold = 1,
+  breakpointCols = 2,
+  className = 'my-masonry-grid',
+  columnClassName = 'my-masonry-grid_column',
+  loader,
+  endMessage,
+  pullDownToRefresh,
+}) => {
+  const { items, refresh, scrollProps } = useInfiniteMasonry(request, {
+    defaultParams: params,
+    ...pagination,
+  });
+
+  useUpdateEffect(() => {
+    refresh();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [JSON.stringify(params)]);
+
+  let pullDownProps: any = {};
+  if (pullDownToRefresh) {
+    pullDownProps = {
+      pullDownToRefresh: true,
+      refreshFunction: refresh,
+      pullDownToRefreshThreshold:
+        typeof pullDownToRefresh === 'object' ? pullDownToRefresh.threshold : 50,
+      pullDownToRefreshContent:
+        typeof pullDownToRefresh === 'object' ? (
+          pullDownToRefresh.content
+        ) : (
+          <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
+        ),
+      releaseToRefreshContent:
+        typeof pullDownToRefresh === 'object' ? (
+          pullDownToRefresh.releaseContent
+        ) : (
+          <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
+        ),
+    };
+  }
+
+  if (pullDownToRefresh) {
+    if (typeof pullDownToRefresh === 'boolean') {
+      pullDownProps = {
+        pullDownToRefresh: true,
+        refreshFunction: refresh,
+        pullDownToRefreshThreshold: 50,
+        pullDownToRefreshContent: (
+          <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
+        ),
+        releaseToRefreshContent: (
+          <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
+        ),
+      };
+    }
+    if (typeof pullDownToRefresh === 'object') {
+      pullDownProps = {
+        pullDownToRefresh: true,
+        refreshFunction: refresh,
+        pullDownToRefreshThreshold: pullDownToRefresh.threshold || 50,
+        pullDownToRefreshContent: pullDownToRefresh.content || (
+          <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
+        ),
+        releaseToRefreshContent: pullDownToRefresh.releaseContent || (
+          <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
+        ),
+      };
+    }
+  }
+
+  return (
+    <InfiniteMasonryScroll
+      masonryProps={{
+        breakpointCols,
+        className,
+        columnClassName,
+      }}
+      scrollThreshold={scrollThreshold}
+      loader={loader}
+      endMessage={endMessage}
+      {...scrollProps}
+      {...pullDownProps}
+    >
+      {items.map(itemRender as any)}
+    </InfiniteMasonryScroll>
+  );
+};
+
+export default InfiniteMasonryScroll;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,35 @@
 /* eslint-disable no-underscore-dangle */
-import InfiniteMasonryScroll, { useInfiniteMasonry } from '@/components/InfiniteMasonryScroll';
+import { ProInfiniteScroll } from '@/components/InfiniteMasonryScroll';
 import { useGlobalContext } from '@/state/global';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getList } from './services';
 
 export default () => {
   // eslint-disable-next-line no-console
   console.log('render home page...');
+  const [params, setP] = useState({ a: 123 });
   const { global } = useGlobalContext();
-  const { items, scrollProps } = useInfiniteMasonry(getList);
   return (
     <div>
       this is home page <Link to="/user/ethan">{global.user.nickname}</Link>
-      <InfiniteMasonryScroll
-        masonryProps={{
-          breakpointCols: 2,
-          className: 'my-masonry-grid',
-          columnClassName: 'my-masonry-grid_column',
+      <button onClick={() => setP((v) => ({ a: v.a + 1 }))}>update params</button>
+      <ProInfiniteScroll<{ title: string }>
+        breakpointCols={1}
+        loader={<div>loading...</div>}
+        request={getList}
+        pagination={{
+          defaultPageSize: 5,
+          defaultCurrent: 3,
         }}
-        {...scrollProps}
-        scrollThreshold={1}
-      >
-        {items.map((el, i) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <div key={i} style={{ height: 200 + i * 10, background: 'grey' }} />
-        ))}
-      </InfiniteMasonryScroll>
+        endMessage={<div>end message</div>}
+        params={params}
+        itemRender={(item, i) => (
+          <div key={i} style={{ height: 200 + i * 10, background: 'grey' }}>
+            {item.title}
+          </div>
+        )}
+      />
     </div>
   );
 };
