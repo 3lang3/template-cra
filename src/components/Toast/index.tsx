7554dfd99fced3c89cd99b3b3424838374b6d071
3lang <675483520@qq.com>
2021-05-17 15:00:51 +0800

    feat: toast component v1 done

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,22 +1,139 @@
 import * as React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
+import { Check, Attention } from '@icon-park/react';
+import loadingSrc from './loading.svg';
+import './toast.css';
 
-function createNotification() {
+interface ToastType {
+  (): void;
+}
+type NoticeType = 'info' | 'success' | 'error' | 'loading';
+type ToastContent = React.ReactNode | string;
+type ToastOnClose = () => void;
+
+/**
+ * toast 全局提示组件
+ *
+ * @todo
+ *  - promisfiy 提供promise接口
+ *  - 对象形式传递参数 eg: toast.info(config)
+ */
+interface ToastInstance {
+  info: (content: ToastContent, duration?: number, onClose?: ToastOnClose) => ToastType;
+  success: (content?: ToastContent, duration?: number, onClose?: ToastOnClose) => ToastType;
+  error: (content: ToastContent, duration?: number, onClose?: ToastOnClose) => ToastType;
+  loading: (content?: ToastContent, duration?: number, onClose?: ToastOnClose) => ToastType;
+  destroy: () => ToastType;
+}
+
+type ToastItemType = {
+  type: NoticeType;
+  content: ToastContent;
+  duration: number;
+  onClose?: ToastOnClose;
+};
+type ToastItemTypeWithKey = { key: React.Key } & ToastItemType;
+
+type AddToastFn = (toast: ToastItemType) => void;
+
+type NotificationRef = {
+  addToast: AddToastFn;
+};
+type NotificationInstance = {
+  addToast: AddToastFn;
+  destroy: () => void;
+};
+
+type ToastItemProps = {
+  content: ToastContent;
+  type: NoticeType;
+};
+const ToastItem = ({ content, type }: ToastItemProps) => {
+  const cls = `${type !== 'info' ? 'p-8' : 'py-2.5 px-5'} local-toast-item`;
+  return (
+    <div className={cls}>
+      {type !== 'info' && (
+        <div className="flex align-middle justify-center mb-2">
+          {(() => {
+            if (type === 'loading') return <img src={loadingSrc} alt="loader" />;
+            if (type === 'success') return <Check theme="outline" size="34" fill="#fff" />;
+            if (type === 'error') return <Attention theme="outline" size="34" fill="#fff" />;
+            return null;
+          })()}
+        </div>
+      )}
+      {content}
+    </div>
+  );
+};
+
+const Toast = React.forwardRef<NotificationRef, any>((_, ref) => {
+  const timerRef = React.useRef<any>();
+  const [toasts, setToasts] = React.useState<ToastItemTypeWithKey[]>([]);
+
+  // 获取唯一key
+  const getKey = () => `local-toast-item-${new Date().getTime()}`;
+
+  // 移除toast
+  const removeToast = (key) => {
+    setToasts((v) =>
+      v.filter((el) => {
+        if (el.key === key && el.onClose) el.onClose();
+        return el.key !== key;
+      }),
+    );
+  };
+
+  // 添加toast
+  const addToast: AddToastFn = (toast) => {
+    const payload = { key: getKey(), ...toast } as ToastItemTypeWithKey;
+    setToasts([payload]); // 永远只显示第一个toast，内容替换
+    if (payload.duration > 0) {
+      if (timerRef.current) clearTimeout(timerRef.current);
+      timerRef.current = setTimeout(() => {
+        removeToast(payload.key);
+      }, payload.duration);
+    }
+  };
+
+  React.useImperativeHandle(ref, () => ({
+    addToast,
+  }));
+
+  React.useEffect(() => {
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <>
+      {toasts.map((el) => (
+        <ToastItem key={el.key} type={el.type} content={el.content} />
+      ))}
+    </>
+  );
+});
+
+function createNotification(): NotificationInstance {
   const container = document.createElement('div');
+  container.id = `local-toast-${new Date().getTime()}`;
+  container.className = 'fixed-center z-99999 local-toast';
   document.body.appendChild(container);
-  let notification;
+  let rootNotification: NotificationRef;
   render(
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
     <Toast
       ref={(ref) => {
-        notification = ref;
+        if (ref) rootNotification = ref;
       }}
     />,
     container,
-  ) as any;
+  );
   return {
-    addToast(toast) {
-      return notification?.addToast(toast);
+    addToast(toast: ToastItemType) {
+      return rootNotification?.addToast(toast);
     },
     destroy() {
       unmountComponentAtNode(container);
@@ -25,58 +142,33 @@ function createNotification() {
   };
 }
 
-const Toast = React.forwardRef<any, any>((_, ref) => {
-  const [toasts, setToasts] = React.useState<any[]>([]);
-  React.useImperativeHandle(ref, () => ({
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    addToast,
-  }));
-  const getKey = () => `local-toast-${new Date().getTime()}`;
-  const removeToast = (key) => {
-    setToasts((v) =>
-      v.filter((el: any) => {
-        if (el.key === key && el.onClose) el.onClose();
-        return el.key !== key;
-      }),
-    );
-  };
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const addToast = (toast) => {
-    const payload = { key: getKey(), ...toast };
-    setToasts([payload]);
-    if (payload.duration > 0) {
-      setTimeout(() => {
-        removeToast(payload.key);
-      }, payload.duration);
-    }
-  };
+let notificationInstance: NotificationInstance | undefined;
 
-  return (
-    <div>
-      {toasts.map((el) => (
-        <div key={el.key}>{el.content}</div>
-      ))}
-    </div>
-  );
-});
-
-let notification;
-const toast = (type, content, duration = 2000, onClose) => {
-  if (!notification) notification = createNotification();
-  return notification.addToast({ type, content, duration, onClose });
+const toast = (
+  type: NoticeType,
+  content: ToastContent,
+  duration = 20000,
+  onClose?: ToastOnClose,
+) => {
+  if (!notificationInstance) notificationInstance = createNotification();
+  return notificationInstance.addToast({ type, content, duration, onClose });
 };
 
 export default {
-  info(content, duration?, onClose?) {
+  info(content, duration, onClose) {
     return toast('info', content, duration, onClose);
   },
-  success(content = '操作成功', duration?, onClose?) {
+  success(content = '操作成功', duration, onClose) {
     return toast('success', content, duration, onClose);
   },
-  error(content, duration?, onClose?) {
+  error(content, duration, onClose) {
     return toast('error', content, duration, onClose);
   },
-  loading(content, duration?, onClose?) {
+  loading(content = '加载中...', duration, onClose) {
     return toast('loading', content, duration, onClose);
   },
-};
+  destroy() {
+    if (notificationInstance) notificationInstance.destroy();
+    notificationInstance = undefined;
+  },
+} as ToastInstance;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,7 +9,17 @@ export default () => {
     <div>
       this is home page 5555 123123 <Link to="/user/ethan">{user.nickname}</Link> <br />
       <div onClick={() => history.push('/login')}>go login</div>
-      <div onClick={() => Toast.info('Welcome')}>toast</div>
+      <div onClick={() => Toast.loading()}>toast loading</div>
+      <div
+        onClick={() =>
+          Toast.info('在页面中间弹出黑色半透明提示，用于消息通知、加载提示、操作结果提示等场景。')
+        }
+      >
+        toast info
+      </div>
+      <div onClick={() => Toast.success()}>toast success</div>
+      <div onClick={() => Toast.error('操作失败')}>toast error</div>
+      <div onClick={() => Toast.destroy()}>toast clean</div>
     </div>
   );
 };
