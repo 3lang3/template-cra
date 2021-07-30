d18e25cbd826fd3d450caac566ea91d085893eea
3lang <675483520@qq.com>
2021-07-30 17:26:45 +0800

    chore: 优化打包后的体积

--- config/config.ts
+++ config/config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import dotenv from 'dotenv';
 import dotenvExpand from 'dotenv-expand';
 import { defineConfig } from 'umi';
@@ -7,15 +8,44 @@ import proxy from './proxy';
 const myEnv = dotenv.config();
 dotenvExpand(myEnv);
 
-const { REACT_APP_ENV = 'dev' } = process.env;
+const { REACT_APP_ENV = 'dev', NODE_ENV } = process.env;
 
 export default defineConfig({
+  hash: NODE_ENV === 'production',
+  externals:
+    NODE_ENV === 'production'
+      ? {
+          react: 'window.React',
+          'react-dom': 'window.ReactDOM',
+        }
+      : false,
+  scripts:
+    NODE_ENV === 'production'
+      ? [
+          // @ts-ignore
+          {
+            src: 'https://unpkg.com/react@17/umd/react.production.min.js',
+          },
+          {
+            src: 'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js',
+          },
+        ]
+      : false,
   routes,
   proxy: (proxy as any)[REACT_APP_ENV],
   nodeModulesTransform: {
     type: 'none',
   },
   request: false,
+  extraBabelPlugins: [
+    [
+      'import',
+      {
+        libraryName: 'react-vant',
+        libraryDirectory: 'es',
+      },
+    ],
+  ],
   extraPostCSSPlugins: [
     require('postcss-aspect-ratio-mini'),
     require('postcss-px-to-viewport')({
@@ -29,7 +59,7 @@ export default defineConfig({
     }),
   ],
   favicon: '/logo.png',
-  // fastRefresh: {},
+  fastRefresh: {},
   dynamicImport: {
     loading: '@/components/Chore/DynamicImportLoader',
   },
